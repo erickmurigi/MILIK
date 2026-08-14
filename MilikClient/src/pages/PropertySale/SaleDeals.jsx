6a1c7d4de870bba83d712b87cd6e5b97b4ebb38b
erickmurigi <ericmurigi04@gmail.com>
2026-08-14 08:51:15 +0300

    vps restart error

--- MilikClient/src/pages/PropertySale/SaleDeals.jsx
+++ MilikClient/src/pages/PropertySale/SaleDeals.jsx
@@ -788,7 +788,6 @@ const SaleDeals = () => {
                 </div>
               </form>
             </div>
-          </div>
 
           {/* Panel footer actions */}
           <div className="flex-shrink-0 border-t border-slate-200 bg-slate-50 px-4 py-2 flex items-center gap-1.5">
